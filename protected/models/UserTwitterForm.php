d4955c21227d49537bc5042f1515a891350d714a
swoop2012 <svoop@bk.ru>
2013-08-13 12:42:12 +0300

    Update UserTwitterForm.php

--- protected/models/UserTwitterForm.php
+++ protected/models/UserTwitterForm.php
@@ -1,8 +1,8 @@
 <?php
 
 /**
- * LoginForm class.
- * LoginForm is the data structure for keeping
+ * UserTwitterForm class.
+ * UserTwitterForm is the data structure for keeping
  * user login form data. It is used by the 'login' action of 'SiteController'.
  */
 class UserTwitterForm extends CFormModel
